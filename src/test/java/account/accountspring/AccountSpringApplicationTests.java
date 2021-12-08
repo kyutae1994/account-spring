d2367fdc906fa5346c6c5a20235392c5d02d2c8f
kyutae <k305059ute@gmail.com>
2021-12-08 23:11:31 +0900

    프로젝트 생성

---
+++ src/test/java/account/accountspring/AccountSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package account.accountspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
